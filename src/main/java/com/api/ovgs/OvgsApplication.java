167b033d1b770c81fed2371c2e5ca9323cbfea66
Caio <caiovilarouca@gmail.com>
2026-09-09 23:20:56 -0300

    feat class item

--- src/main/java/com/api/ovgs/OvgsApplication.java
+++ src/main/java/com/api/ovgs/OvgsApplication.java
@@ -1,35 +1,11 @@
 package com.api.ovgs;
 
-import java.util.Arrays;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.api.ovgs.entity.Client;
-import com.api.ovgs.repository.ClientRepository;
-
 @SpringBootApplication
-public class OvgsApplication  implements CommandLineRunner {	// teste do banco de dados H2
-
-	// teste do banco de dados H2
-	private final ClientRepository clientRepository;
-	OvgsApplication(ClientRepository clientRepository) {
-		this.clientRepository = clientRepository;
-	}	
-
+public class OvgsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OvgsApplication.class, args);
-	}
-
-	@Override 	// teste do banco de dados H2
-
-	public void run(String... args) throws Exception {
-		Client c1 = new Client(null, "Caio Vilarouca", "caiodocs", "caio@email.com", true);
-		Client c2 = new Client(null, "Tongas Felix", "tongasdocs", "tongas@email.com", false);
-
-		clientRepository.saveAll(Arrays.asList(c1,c2));
-	}
-	
-	
+	}	
 }
---
+++ src/main/java/com/api/ovgs/entity/Item.java
@@ -0,0 +1,68 @@
+package com.api.ovgs.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tb_item")
+public class Item {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(nullable = false, length = 150)
+	private String name;
+	
+	@Column(length = 255)
+	private String description;
+	
+	@Column(nullable = false)
+	private boolean active;
+	
+	public Item() {}
+
+	public Item(Integer id, String name, String description, boolean active) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.active = active;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+}
