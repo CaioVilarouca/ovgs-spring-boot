a4cb6d849538892b645aa78dd3144d1e087b784e
Caio <caiovilarouca@gmail.com>
2026-09-09 21:47:13 -0300

    projeto criado

---
+++ src/main/java/com/api/ovgs/OvgsApplication.java
@@ -0,0 +1,13 @@
+package com.api.ovgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvgsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OvgsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/ovgs/OvgsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.ovgs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OvgsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
