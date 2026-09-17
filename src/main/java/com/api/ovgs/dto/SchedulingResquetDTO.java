e2146049504d3923e4f96c5f4128ac3b3cffef9b
Caio <caiovilarouca@gmail.com>
2026-09-17 15:04:24 -0300

    feat = create class SchedulingResquetDTO

---
+++ src/main/java/com/api/ovgs/dto/SchedulingResquetDTO.java
@@ -0,0 +1,40 @@
+package com.api.ovgs.dto;
+
+import com.api.ovgs.domain.StatusScheduling;
+import com.api.ovgs.entity.Scheduling;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class SchedulingResquetDTO {
+
+    private Integer id;
+    private LocalDate dayDelivery;
+    private LocalTime windowsStar;
+    private LocalTime windowsFinish;
+    private StatusScheduling statusScheduling;
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setDayDelivery(LocalDate dayDelivery) {
+        this.dayDelivery = dayDelivery;
+    }
+
+    public void setWindowsStar(LocalTime windowsStar) {
+        this.windowsStar = windowsStar;
+    }
+
+    public void setWindowsFinish(LocalTime windowsFinish) {
+        this.windowsFinish = windowsFinish;
+    }
+
+    public void setStatusScheduling(StatusScheduling statusScheduling) {
+        this.statusScheduling = statusScheduling;
+    }
+
+    public Scheduling toScheduling() {
+        return new Scheduling(null, this.dayDelivery, this.windowsStar, this.windowsFinish, this.statusScheduling);
+    }
+}
