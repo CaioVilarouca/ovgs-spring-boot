a04573576686ee0e9e00fca8cc3bd460ee3ae14f
Caio <caiovilarouca@gmail.com>
2026-09-17 15:16:26 -0300

    feat = create class SchedulingRepository

---
+++ src/main/java/com/api/ovgs/service/SchedulingService.java
@@ -0,0 +1,22 @@
+package com.api.ovgs.service;
+
+import com.api.ovgs.dto.SchedulingReponseDTO;
+import com.api.ovgs.dto.SchedulingResquetDTO;
+import com.api.ovgs.entity.Scheduling;
+import com.api.ovgs.repository.SchedulingRepository;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SchedulingService {
+
+    private final SchedulingRepository schedulingRepository;
+
+    public SchedulingService(SchedulingRepository schedulingRepository) {
+        this.schedulingRepository = schedulingRepository;
+    }
+
+    public SchedulingReponseDTO createNewScheduling(@NotNull SchedulingResquetDTO schedulingResquetDTO){
+        return new SchedulingReponseDTO(schedulingRepository.save(schedulingResquetDTO.toScheduling()));
+    }
+}
